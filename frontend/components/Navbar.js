415e8b69d9b8622cb04a5507f65b3d49f1b25419
Salam Sam A.s <Salam.sam.abdul-samad@umontreal.ca>
2025-01-04 18:56:03 -0500

    Navigate bar and login form

---
+++ frontend/components/Navbar.js
@@ -0,0 +1,19 @@
+// src/components/Navbar.js
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Dashboard</Link></li>
+        <li><Link to="/add-expense">Add Expense</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/register">Register</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
